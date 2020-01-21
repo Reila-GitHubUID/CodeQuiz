8b295a78abb17f153e6f810044c9f259aa643bbb
Ellin Tj <tjellin@gmail.com>
2020-01-21 00:23:11 -0800

    edited script.js

--- script.js
+++ script.js
@@ -3,5 +3,21 @@
 "#viewScore"
 "#startQuiz"
 
-var mainEl = document.getElementById("#counter");
+var counterEl = document.getElementById("#counter");
 let secondsLeft = 75;
+
+// This function is for setting up the count down
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      counterEl.textContent = secondsLeft;
+  
+      // this is to stop the function from looping and start over
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+    // 1000ms is the time to wait before secondsLeft counting down
+  }
